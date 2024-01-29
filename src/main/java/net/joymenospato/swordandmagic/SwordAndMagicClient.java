9c9c22941dc2e8fede46521ae7b849d584442eac
AbnerSLandim <abnergamer2201@gmail.com>
2024-01-28 21:39:56 -0300

    Initial commit

---
+++ src/main/java/net/joymenospato/swordandmagic/SwordAndMagicClient.java
@@ -0,0 +1,10 @@
+package net.joymenospato.swordandmagic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwordAndMagicClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
